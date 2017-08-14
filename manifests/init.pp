300b4d4723159e63853965202e70f2f639f5de2d
Steve Clatterbuck <clattems@Steves-MacBook-Pro.local>
2017-08-14 12:41:27 -0500

    initial push

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class standards {
+  package { 'sharutils':
+    ensure => installed,
+  }
+
+  package { 'sysstat':
+    ensure => installed,
+  }
+  file { '/etc/profile.d/ps1.sh':
+    ensure => file,
+    mode => '0644',
+    source => 'puppet:///standards/ps1.sh',
+  }
+}
